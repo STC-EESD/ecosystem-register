2640ee8e87b249ca7938cbb3d3123125a2adc633
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2023-02-19 10:05:58 -0500

    updated: 901-rHEALPix/code/main.py

--- 901-rHEALPix/code/main.py
+++ 901-rHEALPix/code/main.py
@@ -40,7 +40,13 @@ logging.basicConfig(filename='log.debug',level=logging.DEBUG)
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 from rhealpixdggs.ellipsoids import *  
+from rhealpixdggs.dggs       import *
 from rhealpixdggs            import dggs
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+print("WGS84_A:" + str(WGS84_A))
+print("WGS84_F:" + str(WGS84_F))
+print("WGS84_E:" + str(WGS84_E))
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 ellps_1 = Ellipsoid(a=5, e=0.8) 
@@ -55,14 +61,18 @@ rdggs = dggs.WGS84_003
 print(rdggs)
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-ellps_1 = Ellipsoid(a=WGS84_A, f=WGS84_F, radians=False, lon_0=-50)
-print(ellps_1)
+WGS84_minus50 = Ellipsoid(a=WGS84_A, f=WGS84_F, radians=False, lon_0=-50)
+print(WGS84_minus50)
+
+rHEALPixCanada = RHEALPixDGGS(
+    ellipsoid    = WGS84_minus50,
+    north_square = 0,
+    south_square = 0,
+    N_side       = 3
+    )
+print(rHEALPixCanada)
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-print(WGS84_A)
-print(WGS84_F)
-print(WGS84_E)
-
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 
