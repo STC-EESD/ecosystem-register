7655c54304b7f47a599f3ccf0c198830c54ad0bd
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2023-02-19 00:33:27 -0500

    added: 901-rHEALPix/

---
+++ 901-rHEALPix/code/main.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import os, sys, shutil, getpass
+import pprint, logging, datetime
+import stat
+
+dir_data            = os.path.realpath(sys.argv[1])
+dir_code            = os.path.realpath(sys.argv[2])
+dir_output          = os.path.realpath(sys.argv[3])
+google_drive_folder = sys.argv[4]
+
+if not os.path.exists(dir_output):
+    os.makedirs(dir_output)
+
+os.chdir(dir_output)
+
+myTime = "system time: " + datetime.datetime.now().strftime("%c")
+print( "\n" + myTime + "\n" )
+
+print( "\ndir_data: "            + dir_data            )
+print( "\ndir_code: "            + dir_code            )
+print( "\ndir_output: "          + dir_output          )
+print( "\ngoogle_drive_folder: " + google_drive_folder )
+
+print( "\nos.environ.get('GEE_ENV_DIR'):")
+print(    os.environ.get('GEE_ENV_DIR')  )
+
+print( "\n### python module search paths:" )
+for path in sys.path:
+    print(path)
+
+print("\n####################")
+
+logging.basicConfig(filename='log.debug',level=logging.DEBUG)
+
+##################################################
+##################################################
+# import seaborn (for improved graphics) if available
+# import seaborn as sns
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+from rhealpixdggs.ellipsoids import *  
+from rhealpixdggs            import dggs
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+ellps_1 = Ellipsoid(a=5, e=0.8) 
+print(ellps_1)
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+E = WGS84_ELLIPSOID 
+print(E)
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+rdggs = dggs.WGS84_003 
+print(rdggs)
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+ellps_1 = Ellipsoid(a=WGS84_A, f=WGS84_F, radians=False, lon_0=-50)
+print(ellps_1)
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+print(WGS84_A)
+print(WGS84_F)
+print(WGS84_E)
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+
+##################################################
+##################################################
+print("\n####################\n")
+myTime = "system time: " + datetime.datetime.now().strftime("%c")
+print( myTime + "\n" )
